1d4c7b2b32c1acf8447625752e6c5ff3a934a0ee
frsusana <susana.fernandez.redondo@alumnos.upm.es>
2018-05-19 12:27:27 +0200

    Add files via upload

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+var partials = require('express-partials');//partials
 var indexRouter = require('./routes/index');
 
 var app = express();
@@ -12,6 +13,7 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'js');
 
+app.use(partials()); //partials
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
